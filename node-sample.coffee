8bf54370838ab7103c4b491d05bcac397067c238
Nick Retallack <nickretallack@gmail.com>
2011-09-29 05:38:59 -0700

    removed unimportant thing

--- node-sample.coffee
+++
@@ -1,4 +0,0 @@
-express = require 'express'
-app = express.createServer()
-app.use express.static __dirname
-app.listen 5000
--- node-sample.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  var app, express;
-  express = require('express');
-  app = express.createServer();
-  app.use(express.static(__dirname));
-  app.listen(5000);
-}).call(this);
